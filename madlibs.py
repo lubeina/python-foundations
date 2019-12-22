6fec4153100ba084f85af8783d9e22b002a6f1cd
lubeina <52536093+lubeina@users.noreply.github.com>
2019-12-22 20:30:09 +0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -7,4 +7,4 @@ name = input("Enter a name:")
 scream = input("Enter any sentence:")
 action = input("Enter a verb:")
 
-print ("The story then goes... \nIt was "+time+" o'clock when I heard a nock at the door. \nI opened the door and there was a box full of "+items+" with a note saying 'From "+name.title()+".' \nJust as I closed the door I heard a scream '"+scream.upper()+".' \nI froze in place and all I could do was shake my head.")
+print ("The story then goes... \nIt was "+time+" o'clock when I heard a nock at the door. \nI opened the door and there was a box full of "+items+" with a note saying 'From "+name.title()+".' \nJust as I closed the door I heard a scream '"+scream.upper()+".' \nI froze in place and all I could do was "+action+".")
